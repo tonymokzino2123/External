3d828ba4cbca00362ee72dbe36867eeed6dc4788
tonymokzino2123 <151216052+tonymokzino2123@users.noreply.github.com>
2023-11-17 13:01:30 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "your_server_name";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database_name";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
